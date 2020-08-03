0641a78548480c2eb79da6ad941629d690702154
prodiptahalder <59222228+prodiptahalder@users.noreply.github.com>
2020-08-03 03:00:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+(function ($) {
+    'use strict';
+
+    $('.cv').click(() => {
+        $('.tab').addClass('cover');
+        $('.cv').addClass('lg-sm');
+        setTimeout(function(){
+            $('.tab').addClass('hide')
+            $('.cvmaker').removeClass('hide')
+        },485)
+    });
+
+    
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+    $('.hero-slider').slick({
+		autoplay: true,
+		autoplaySpeed: 7500,
+		pauseOnFocus: false,
+		pauseOnHover: false,
+		infinite: true,
+		arrows: true,
+		fade: true,
+		prevArrow: '<button type=\'button\' class=\'prevArrow\'><i class=\'ti-angle-left\'></i></button>',
+		nextArrow: '<button type=\'button\' class=\'nextArrow\'><i class=\'ti-angle-right\'></i></button>',
+		dots: true
+	});
+	// $('.hero-slider').slickAnimation();
+})(jQuery);
